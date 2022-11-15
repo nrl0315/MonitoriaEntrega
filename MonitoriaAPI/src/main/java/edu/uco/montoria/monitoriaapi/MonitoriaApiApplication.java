045c5b607438049d9b0b42a7d27c734e7ef97ae1
nrl0315 <nicolas.rendon5948@soyuco.edu.co>
2022-11-15 12:00:48 -0500

    Nov 15 4th commit

--- MonitoriaAPI/src/main/java/edu/uco/montoria/monitoriaapi/MonitoriaApiApplication.java
+++ MonitoriaAPI/src/main/java/edu/uco/montoria/monitoriaapi/MonitoriaApiApplication.java
@@ -2,8 +2,10 @@ package edu.uco.montoria.monitoriaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"edu.uco.monitoria"})
 public class MonitoriaApiApplication {
 
     public static void main(String[] args) {
